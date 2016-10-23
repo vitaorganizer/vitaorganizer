eab4c0e9301ae6a9092acb4fb05b5081499a91c9
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-10-23 11:56:49 +0200

    To be consistent, we have to use a specific locale. Check that different locales produce the expected result

--- test/com/soywiz/vitaorganizer/FileSizeTest.kt
+++ test/com/soywiz/vitaorganizer/FileSizeTest.kt
@@ -1,16 +1,21 @@
 package com.soywiz.vitaorganizer
 
+import com.soywiz.vitaorganizer.ext.setTemporarily
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 
-class FileFileSize {
+class FileSizeTest {
 	@Test
 	fun testDefaultFileSize() {
 		Assert.assertEquals("0 B", FileSize.toString(0L))
 		Assert.assertEquals("1 KB", FileSize.toString(1024L))
 		Assert.assertEquals("121 KB", FileSize.toString(123456L))
-		Assert.assertEquals("11,8 MB", FileSize.toString(12345678L))
-		Assert.assertEquals("11,498 GB", FileSize.toString(12345678901L))
-		Assert.assertEquals("1,122833 TB", FileSize.toString(1234567890123L))
+		Assert.assertEquals("11.8 MB", FileSize.toString(12345678L))
+		Assert.assertEquals("11.498 GB", FileSize.toString(12345678901L))
+		Assert.assertEquals("1.122833 TB", FileSize.toString(1234567890123L))
 	}
+
+	@Test fun testDefaultFileSizeEnglishLocale() = Locale.ENGLISH.setTemporarily { testDefaultFileSize() }
+	@Test fun testDefaultFileSizeSpanishLocale() = Locale.forLanguageTag("es").setTemporarily { testDefaultFileSize() }
 }
