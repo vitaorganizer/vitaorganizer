c50925f3a28292f6af72a51ed7f1c8c3d7a0a2ee
gordon0001 <gordon0001@users.noreply.github.com>
2016-12-06 21:21:02 +0100

    Move software version related stuff to VitaOrganizerVersion
    Seems to be a better place

--- src/com/soywiz/vitaorganizer/VitaOrganizerVersion.kt
+++ src/com/soywiz/vitaorganizer/VitaOrganizerVersion.kt
@@ -1,7 +1,66 @@
 package com.soywiz.vitaorganizer
 
 import com.soywiz.vitaorganizer.ext.getResourceString
+import java.net.URL
 
 object VitaOrganizerVersion {
 	val currentVersion: String get() = getResourceString("com/soywiz/vitaorganizer/currentVersion.txt") ?: "unknown"
+	val lastVersion: String get() =  URL("https://raw.githubusercontent.com/soywiz/vitaorganizer/master/lastVersion.txt").readText()
+
+	var lastVersionUrl: String? = null
+
+	fun getCurrentVersion() : SoftwareVersion = parseVersion(currentVersion)
+
+	fun getLastVersion() : SoftwareVersion	{
+		if(lastVersion.isEmpty())
+			throw Exception("No content received from update URL")
+
+		val parts = lastVersion.lines()
+		if(parts.count() < 2)
+			throw Exception("Unexpected content")
+
+		lastVersionUrl = parts[1]
+		return parseVersion(parts[0])
+	}
+
+	private fun parseVersion(versionString: String) : SoftwareVersion {
+		val regexVersion =Regex("([0-9]{1,2})\\.([0-9]{1,2})\\.([0-9]{1,2})")
+		val matchVersion = regexVersion.matchEntire(versionString)
+
+		if(matchVersion == null) {
+			println("not matched")
+			throw Exception("Invalid version string");
+		}
+
+		val versionGroupValues = matchVersion.groupValues;
+		return SoftwareVersion(
+			versionGroupValues[1].toByte(),
+			versionGroupValues[2].toByte(),
+			versionGroupValues[3].toByte())
+	}
 }
+
+class SoftwareVersion(val major: Byte, val minor: Byte, val revision: Byte) {
+
+	fun print(prefix: String) {
+		println("[$prefix] Major($major) Minor($minor) Revision($revision)")
+	}
+
+	fun didIncrement(version: SoftwareVersion) : Boolean {
+		if(version.major < this.major) return true;
+		if(version.minor < this.minor) return true;
+		if(version.revision < this.revision) return true;
+		return false;
+	}
+
+	fun equals(version: SoftwareVersion) : Boolean {
+		if(version.major == this.major
+		   && version.minor == this.minor
+		   && version.revision == this.revision)
+			return true
+		return false
+	}
+
+	override fun toString() : String = "${major}.${minor}.${revision}"
+}
+
--- src/com/soywiz/vitaorganizer/tasks/CheckForUpdatesTask.kt
+++ src/com/soywiz/vitaorganizer/tasks/CheckForUpdatesTask.kt
@@ -9,63 +9,8 @@ import java.net.URL
 class CheckForUpdatesTask(vitaOrganizer: VitaOrganizer, val showCurrentVersionDialog: Boolean = true) : VitaTask(vitaOrganizer) {
 	override fun perform() {
 		try {
-			val text = URL("https://raw.githubusercontent.com/soywiz/vitaorganizer/master/lastVersion.txt").readText()
-			if(text.isEmpty())
-				throw Exception("No content received from update URL")
-
-			val parts = text.lines()
-			if(parts.count() < 2)
-				throw Exception("Unexpected content")
-
-			val lastVersion = parts[0]
-			val lastVersionUrl = parts[1]
-
-			val regexVersion =Regex("([0-9]{1,2})\\.([0-9]{1,2})\\.([0-9]{1,2})")
-			val matchLastVersion = regexVersion.matchEntire(lastVersion)
-			val matchCurrentVersion = regexVersion.matchEntire( VitaOrganizerVersion.currentVersion)
-
-			if(matchLastVersion == null || matchCurrentVersion == null) {
-				println("not matched")
-				throw Exception("Invalid version strings");
-			}
-
-			class SoftwareVersion(val major: Byte, val minor: Byte, val revision: Byte) {
-
-				fun print(prefix: String) {
-					println("[$prefix] Major($major) Minor($minor) Revision($revision)")
-				}
-
-				fun didIncrement(version: SoftwareVersion) : Boolean {
-					if(version.major < this.major) return true;
-					if(version.minor < this.minor) return true;
-					if(version.revision < this.revision) return true;
-					return false;
-				}
-
-				fun equals(version: SoftwareVersion) : Boolean {
-					if(version.major == this.major
-					&& version.minor == this.minor
-					&& version.revision == this.revision)
-						return true
-					return false
-				}
-
-				override fun toString() : String = "${major}.${minor}.${revision}"
-			}
-
-			val lastVersionGroupValues = matchLastVersion.groupValues;
-			val lastSoftwareVersion = SoftwareVersion(
-				lastVersionGroupValues[1].toByte(),
-				lastVersionGroupValues[2].toByte(),
-				lastVersionGroupValues[3].toByte()
-			)
-
-			val currentVersionGroupValues = matchCurrentVersion.groupValues;
-			val currentSoftwareVersion = SoftwareVersion(
-				currentVersionGroupValues[1].toByte(),
-				currentVersionGroupValues[2].toByte(),
-				currentVersionGroupValues[3].toByte()
-			)
+			val lastSoftwareVersion = VitaOrganizerVersion.getLastVersion()
+			val currentSoftwareVersion = VitaOrganizerVersion.getCurrentVersion()
 
 			lastSoftwareVersion.print("Last")
 			currentSoftwareVersion.print("Current")
@@ -86,13 +31,12 @@ class CheckForUpdatesTask(vitaOrganizer: VitaOrganizer, val showCurrentVersionDi
 					Texts.format("NEW_VERSION_AVAILABLE", "lastVersion" to lastSoftwareVersion.toString(), "currentVersion" to currentSoftwareVersion.toString())
 				)
 				if (result)
-					openWebpage(URL(lastVersionUrl))
+					openWebpage(URL(VitaOrganizerVersion.lastVersionUrl))
 			}
 			else {
 				println("decreased/changed?")
 				throw Exception("Invalid")
 			}
-			println(parts)
 		}
 		catch(e: java.net.UnknownHostException) {
 			error("${Texts.format("UPDATE_CHECK_FAILED")} ${Texts.format("UNKNOWN_REMOTE_HOST")} ${e.message!!}")
