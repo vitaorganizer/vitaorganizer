ebe735f25325ae2d7776322ba4ba6224833f24e6
gordon0001 <gordon0001@users.noreply.github.com>
2016-12-07 17:47:04 +0100

    Test whatever we are currently using.
    KB,MB,GB,TB (base 10) or KiB,MiB,GiB,TiB (base 2, windows uses base 2 but shows KB,MB,GB, etc)
    We also use base 2 on windows to dont confuse people, but we are using the correct units
    see also https://en.wikipedia.org/wiki/Kilobyte

--- test/com/soywiz/vitaorganizer/FileSizeTest.kt
+++ test/com/soywiz/vitaorganizer/FileSizeTest.kt
@@ -8,12 +8,23 @@ import java.util.*
 class FileSizeTest {
 	@Test
 	fun testDefaultFileSize() {
-		Assert.assertEquals("0 B", FileSize.toString(0L))
-		Assert.assertEquals("1 KB", FileSize.toString(1024L))
-		Assert.assertEquals("121 KB", FileSize.toString(123456L))
-		Assert.assertEquals("11.8 MB", FileSize.toString(12345678L))
-		Assert.assertEquals("11.498 GB", FileSize.toString(12345678901L))
-		Assert.assertEquals("1.122833 TB", FileSize.toString(1234567890123L))
+
+		if(VitaOrganizerSettings.unitBase == 10) {
+			Assert.assertEquals("0 B", FileSize.toString(0L))
+			Assert.assertEquals("1 kB", FileSize.toString(1000L))
+			Assert.assertEquals("121 kB", FileSize.toString(121000L))
+			Assert.assertEquals("11.8 MB", FileSize.toString(11800000L))
+			Assert.assertEquals("11.498 GB", FileSize.toString(11498000000L))
+			Assert.assertEquals("1.122833 TB", FileSize.toString(112283300000L))
+		}
+		else if(VitaOrganizerSettings.unitBase == 2){
+			Assert.assertEquals("0 B", FileSize.toString(0L))
+			Assert.assertEquals("1 KiB", FileSize.toString(1024L))
+			Assert.assertEquals("121 KiB", FileSize.toString(123456L))
+			Assert.assertEquals("11.8 MiB", FileSize.toString(12345678L))
+			Assert.assertEquals("11.498 GiB", FileSize.toString(12345678901L))
+			Assert.assertEquals("1.122833 TiB", FileSize.toString(1234567890123L))
+		}
 	}
 
 	@Test fun testDefaultFileSizeEnglishLocale() = Locale.ENGLISH.setTemporarily { testDefaultFileSize() }
